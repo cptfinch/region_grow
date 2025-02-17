e1e507b79f9ee919e880c5bb0877b99d54e97998
Christopher Finch <ahfjf@goiba.net>
2025-02-17 09:50:03 +0000

    update

---
+++ c_practice/cmake-build-debug/CMakeFiles/c_practice.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/c_practice.dir/main.c.o"
+  "c_practice.pdb"
+  "c_practice"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/c_practice.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
